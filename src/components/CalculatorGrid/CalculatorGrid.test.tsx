76e1d78d80c9902d26287032e93146e7256eca8f
Adeola Adekoyejo <adekoyejoadeola@gmail.com>
2022-12-22 10:45:23 +0000

    fix: removed unused code

--- src/components/CalculatorGrid/CalculatorGrid.test.tsx
+++ src/components/CalculatorGrid/CalculatorGrid.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import CalculatorGrid from './CalculatorGrid';
 
 describe('<CalculatorGrid/>', () => {
